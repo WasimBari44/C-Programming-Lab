c74197e912b4592e57f775cfcefbb9b2ca89c6ab
WasimBari44 <mdw94153@gmail.com>
2026-07-25 02:23:23 +0600

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {10, 20, 30, 40, 50};
+    int i, sum = 0;
+    float mean;
+
+    for(i = 0; i < 5; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    mean = (float)sum / 5;
+    printf("Mean = %.2f", mean);
+
+    return 0;
+}
