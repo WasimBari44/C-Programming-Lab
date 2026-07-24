2a0584ecc042a8ce6c4bd1b40e422334f5923262
WasimBari44 <mdw94153@gmail.com>
2026-07-25 02:26:46 +0600

    Add files via upload

---
+++ camel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int firstNumber=50;
+    int secondNumber=60;
+    int totalSum;
+
+    totalSum=firstNumber+secondNumber;
+
+    printf("Total Sum=%d",totalSum);
+
+    return 0;
+}
