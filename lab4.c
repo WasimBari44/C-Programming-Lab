d90b5ffcf218bc82d5b7cc6673aaa155ef0968ea
WasimBari44 <mdw94153@gmail.com>
2026-07-25 02:25:33 +0600

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int first_number=50;
+    int second_number=60;
+    int total_sum;
+
+    total_sum=first_number+second_number;
+
+    printf("Total Sum =%d",total_sum);
+
+    return 0;
+}
