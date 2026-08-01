95794c6b92002ff6040309fe954c5514cf9d342e
WasimBari44 <mdw94153@gmail.com>
2026-08-01 11:16:47 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+// Function to check prime
+int isPrime(int num)
+{
+    int i;
+
+    if (num < 2)
+        return 0;
+
+    for (i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n, i, count = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers are:\n");
+
+    for (i = 1; i <= n; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+            count++;
+        }
+    }
+
+    printf("\nTotal Prime = %d\n", count);
+
+    return 0;
+}
